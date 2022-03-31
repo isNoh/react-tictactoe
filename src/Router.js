1093459caaf0bc90e8468c9e69d8efd448553c23
isNoh <98067131+isNoh@users.noreply.github.com>
2022-03-31 10:59:39 +0900

    gh-pages 재 수정

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import App from "./App";
 
 export default () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Route exact path="/">
+      <App />
+    </Route>
   );
 };
